2e68139f1d3db10aeefd7e719b21d8ea9b6bde92
Vinayak Tiwari <84216407+VinayakTiwari1103@users.noreply.github.com>
2025-01-21 21:47:08 +0530

    Update variables.tf
    IMP commit for the CLONERS

--- variables.tf
+++ variables.tf
@@ -3,7 +3,7 @@ variable "bucket_name" {
   description = "TeraPRO1103"
 }
 
-
+// you should your own bucket name and your own ownership 
 variable "object_ownership" {
   type        = string
   description = "BucketOwnerPreferred"
